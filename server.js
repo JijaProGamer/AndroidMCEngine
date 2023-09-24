be8939b936d45231ee7397b238eed60a11620e45
John Doe <johndoe@email.com>
2023-09-24 15:49:39 +0300

    init

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ const path = require('path');
 const app = express();
 const port = 8085;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'web', 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', "index.html"));
+    res.sendFile(path.join(__dirname, 'web', 'dist', "index.html"));
 });
 
 app.get('/:file', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', req.params.file));
+    res.sendFile(path.join(__dirname, 'web', 'dist', req.params.file));
 });
 
 app.get('/assets/:file', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'assets', req.params.file));
+    res.sendFile(path.join(__dirname, 'web', 'dist', 'assets', req.params.file));
 });
 
 app.listen(port, () => {
