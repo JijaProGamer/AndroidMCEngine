11cdf3ddaed32d29345f37c9bb19f8ff22863989
John Doe <johndoe@email.com>
2023-09-24 16:30:17 +0300

    switched svelte implementation

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,7 +1,5 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+const config = {
+  
+};
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+export default config;
